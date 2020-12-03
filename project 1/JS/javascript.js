6d280ca45679f4490b052ef1f9ec420a27f45ef3
crowpeckedsnow <m.finck@protonmail.com>
2020-12-03 14:57:55 +0800

    buttons and thumbnail gallery
    added buttons to change header background color and a thumbnail viewer gallery

--- project 1/JS/javascript.js
+++ project 1/JS/javascript.js
@@ -1,1 +1,9 @@
-function
+function backgroundcolor(){
+  document.getElementById('bg').style.backgroundColor='hotpink';
+}
+function originalcolor(){
+  document.getElementById('bg').style.backgroundColor='red';
+}
+function bigview(element){
+  document.getElementById('viewport').style.backgroundImage="url(" + element.src + ")";
+}
