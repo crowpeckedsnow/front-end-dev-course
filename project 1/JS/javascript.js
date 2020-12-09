6503de93a20df120fd94cb336d5f6c691a480e69
crowpeckedsnow <m.finck@protonmail.com>
2020-12-09 13:01:14 +0800

    orderlist, form, and product viewer

--- project 1/JS/javascript.js
+++ project 1/JS/javascript.js
@@ -6,4 +6,33 @@ function originalcolor(){
 }
 function bigview(element){
   document.getElementById('viewport').style.backgroundImage="url(" + element.src + ")";
+  document.getElementById('viewport').innerHTML="";
+}
+function unDo(){
+  document.getElementById('viewport').style.backgroundImage="";
+  document.getElementById('viewport').innerHTML="This is where we view products";
+}
+var orders = ["cotton candy", "gumdrops", "lollipops"];
+var i=0;
+var orderlist="<ul>";
+
+function loadOrders(){
+  for (i=0; i < orders.length; i++){
+    orderlist += "<li>" + orders[i] + "</li>";
+  }
+  document.getElementById("orders").innerHTML = orderlist;
+}
+function myfunction(){
+  var order = prompt("What would you like to order?");
+  orderlist += "<li>" + order + "</li>";
+  document.getElementById("orders").innerHTML = orderlist;
+}
+
+function check(){
+  var email1=document.getElementById('email1');
+  var email2=document.getElementById('email2');
+  if (email1.value != email2.value){
+    alert("The two emails do not match");
+    return false;
+  }
 }
--- project 1/JS/test.js
+++ project 1/JS/test.js
@@ -6,4 +6,11 @@ function originalcolor(){
 }
 function bigview(element){
   document.getElementById('viewport').style.backgroundImage="url(" + element.src + ")";
+  document.getElementById('viewport').style.border="none"
+  document.getElementById('viewport').innerHTML="";
+}
+
+function clear(){
+  document.getElementById('viewport').style.backgroundImage="url('')";
+  document.getElementById('viewport').innerHTML="This is where we view products";
 }
