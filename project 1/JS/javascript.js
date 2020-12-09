d4939986d69fd47201a9829005bc11d272aa9ea9
crowpeckedsnow <m.finck@protonmail.com>
2020-12-09 13:20:24 +0800

    nickname checkbox
    disappears and reappears

--- project 1/JS/javascript.js
+++ project 1/JS/javascript.js
@@ -36,3 +36,13 @@ function check(){
     return false;
   }
 }
+function nicknameFunction(){
+  if (document.getElementById('yesNick').checked){
+    document.getElementById('nick').style.display="inline";
+    document.getElementById('nickname').setAttribute('required', true);
+  }
+  else{
+    document.getElementById('nick').style.display="none";
+    document.getElementById('nickname').removeAttribute('required');
+  }
+}
